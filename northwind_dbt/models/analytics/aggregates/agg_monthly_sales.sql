8f63df393d5ebbb53e1f3ff0b3865909848026ee
vitor <vitorgosmao@gmail.com>
2026-06-19 15:19:35 -0300

    agregações por periodo

---
+++ northwind_dbt/models/analytics/aggregates/agg_monthly_sales.sql
@@ -0,0 +1,9 @@
+select
+    date_trunc('month', order_date) as sales_month,
+    count(distinct order_id) as total_orders,
+    count(*) as total_items,
+    sum(quantity) as total_quantity,
+    sum(sale_amount) as total_revenue
+from 
+    {{ ref('fct_order  _items') }}
+group by 1
